bc81bbfc8bf4eb6e8ae9f25adaa73ecf4d1b6dea
Cary Robbins <carymrobbins@gmail.com>
2017-10-15 23:59:11 -0500

    Optimize updating etaRuntime classpath

--- src/main/groovy/com/typelead/gradle/eta/tasks/EtaRuntime.java
+++ src/main/groovy/com/typelead/gradle/eta/tasks/EtaRuntime.java
@@ -4,23 +4,17 @@ import com.typelead.gradle.eta.plugins.EtaPlugin;
 import com.typelead.gradle.utils.EtlasCommand;
 import org.gradle.api.tasks.TaskAction;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class EtaRuntime extends AbstractEtlasTask {
 
     @TaskAction
     public void runtimeEta() {
-        getRuntimeDependencies().forEach(it -> {
-                getLogger().info("Adding runtimeEta dependency: " + it);
-                getProject().getDependencies().add(
-                        EtaPlugin.ETA_RUNTIME_CONFIGURATION_NAME,
-                        getProject().files(it)
-                );
-        });
-    }
-
-    private Set<String> getRuntimeDependencies() {
-        return new HashSet<>(new EtlasCommand(this).depsClasspath());
+        List<String> etaRuntimeClasspath = new EtlasCommand(this).depsClasspath();
+        getLogger().info("Adding to etaRuntime configuration: " + etaRuntimeClasspath);
+        getProject().getDependencies().add(
+                EtaPlugin.ETA_RUNTIME_CONFIGURATION_NAME,
+                getProject().files(etaRuntimeClasspath.toArray())
+        );
     }
 }
